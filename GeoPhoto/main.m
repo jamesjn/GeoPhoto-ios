49f99efbd1eb34a6b8ab18119d290f4626c8d234
James <jc582@optonline.net>
2012-04-28 18:15:39 -0400

    Initial Commit

---
+++ GeoPhoto/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GeoPhoto
+//
+//  Created by James Chiang on 4/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GeoPhotoAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GeoPhotoAppDelegate class]));
+    }
+}
